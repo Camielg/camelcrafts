ba731be41f9ceccaa6a46cca04c6cae6fa3282dc
camiel gebbink <camielgebbink@gmail.com>
2022-09-12 16:17:34 +0200

    Initial commit

---
+++ src/main/java/nl/camelcrafts/camelcrafts/CamelcraftsApplication.java
@@ -0,0 +1,13 @@
+package nl.camelcrafts.camelcrafts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelcraftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelcraftsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/camelcrafts/camelcrafts/CamelcraftsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.camelcrafts.camelcrafts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelcraftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
